149d6e7203f3bfc7bb410d98496b067d4e43af51
anirudhreddy7 <anirudhr424@gmail.com>
2016-06-17 18:29:52 +0000

    Add files via upload

---
+++ Test my/b.js
@@ -0,0 +1,10 @@
+function myFunction(){
+		alert("I'm an alert box");
+
+}
+function inFunc() {
+	document.getElementById("demo").innerHTML = "second alert";
+}
+function inFunc1() {
+	alert("scrum")
+}
